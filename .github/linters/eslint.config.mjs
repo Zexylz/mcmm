e122532512fb687d5fc7ea8fad06f5a00c0050b5
Zonk1987 <s-haupt@hotmail.de>
2026-01-02 18:07:25 +0100

    try to make this cleaner

--- .github/linters/eslint.config.mjs
+++ .github/linters/eslint.config.mjs
@@ -2,18 +2,17 @@ import js from "@eslint/js";
 import globals from "globals";
 
 export default [
-    js.configs.recommended,
-    {
-        files: ["**/*.js"],
-        languageOptions: {
-            ecmaVersion: 12,
-            sourceType: "module",
-            globals: {
-                ...globals.browser,
-                ...globals.es2021
-            }
-        },
-        rules: {
-        }
-    }
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    rules: {},
+  },
 ];
